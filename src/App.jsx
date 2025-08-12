94ba276a6a5ba87c17ca7751b5b25cf8bd1e2871
hassan2282 <taghavey.hassan@gmail.com>
2025-08-13 01:01:36 +0330

    install react kit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './index.css'
+import { FaBeer } from 'react-icons/fa';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,6 +28,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
+        <FaBeer />
         Click on the Vite and React logos to learn more
       </p>
     </>
