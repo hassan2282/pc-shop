9d4abd42bc090535e6a8e2926213b4cc167a55cc
hassan2282 <taghavey.hassan@gmail.com>
2025-09-22 01:06:26 +0330

    edit apiClient

--- src/apiClient.js
+++ src/apiClient.js
@@ -2,13 +2,12 @@ import axios from 'axios';
 import { logoutAction } from './Actions';
 import store from './store';
 
-// ✅ اول apiClient رو بساز
+
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // آدرس پایه API
-  timeout: 10000, // زمان انتظار
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 10000, 
 });
 
-// 🔍 Interceptor برای درخواست‌ها (افزودن هدر Authorization)
 apiClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
@@ -20,7 +19,7 @@ apiClient.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
-// ⚠️ Interceptor برای پاسخ‌ها (مدیریت خطاهای 401 و رفرش توکن)
+
 apiClient.interceptors.response.use(
   (response) => response,
   async (error) => {
@@ -28,18 +27,20 @@ apiClient.interceptors.response.use(
     if (error.response && error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
       try {
-        const res = await apiClient.post("refresh");
-        const newToken = res.data.authorisation.token;
+        const res = await apiClient.post("auth/refresh");
+        const newToken = res.data.access_token;
         localStorage.setItem("token", newToken);
         apiClient.defaults.headers.Authorization = `Bearer ${newToken}`;
         originalRequest.headers.Authorization = `Bearer ${newToken}`;
-        return apiClient(originalRequest); // تکرار درخواست اصلی
+        return apiClient(originalRequest);
       } catch (err) {
+        console.log(err);
         store.dispatch(logoutAction());
       }
     }
     return Promise.reject(error);
   }
 );
+
 
 export default apiClient;
