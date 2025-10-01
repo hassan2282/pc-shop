7cf8e46fa6862e461c282b718fa3b09bb5de9ccd
hassan2282 <taghavey.hassan@gmail.com>
2025-10-01 23:19:16 +0330

    debug

---
+++ src/AdminJs.js
@@ -0,0 +1,8 @@
+import './AdminPanel/assets/js/backend-bundle.min.js';
+import './AdminPanel/assets/js/table-treeview.js';
+import './AdminPanel/assets/js/customizer.js';
+import './AdminPanel/assets/js/chart-custom.js';
+import './AdminPanel/assets/js/charts/01.js';
+import './AdminPanel/assets/js/charts/02.js';
+import './AdminPanel/assets/js/slider.js';
+import './AdminPanel/assets/js/app.js';
