d37513b7e66d1990ed47caad60ebd32016a95706
hassan2282 <taghavey.hassan@gmail.com>
2025-10-06 13:11:47 +0330

    import css files from index.html to StorePannel

--- src/StorePanel/structure/StoreContainer.jsx
+++ src/StorePanel/structure/StoreContainer.jsx
@@ -3,6 +3,23 @@ import Header from './Header'
 import Footer from './Footer'
 import ScrollToTop from './ScrollToTop'
 
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+
+import '../assets/css/main_ui.css';
+import '../assets/css/icon.css';
+import '../assets/css/main.css?1';
+import '../assets/css/style.css';
+
+
+
+import 'bootstrap';
+import 'owl.carousel';
+
+
 function StoreContainer() {
 
   return (
