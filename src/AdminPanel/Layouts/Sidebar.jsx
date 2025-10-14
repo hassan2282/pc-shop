ef2321f6cd6e0f179e0ef436b8168f7e34f94779
hassan2282 <taghavey.hassan@gmail.com>
2025-10-14 18:37:22 +0330

    edit admin ui

--- src/AdminPanel/Layouts/Sidebar.jsx
+++ src/AdminPanel/Layouts/Sidebar.jsx
@@ -1,54 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { LiaDollyFlatbedSolid } from 'react-icons/lia'
-import { TbArticle, TbChartBarPopular, TbEye, TbHome, TbMail, TbMenu2, TbSearch, TbUserCog } from 'react-icons/tb'
+import { TbArticle, TbCategoryFilled, TbChartBarPopular, TbEye, TbHome, TbMail, TbMenu2, TbSearch, TbUserCog, TbX } from 'react-icons/tb'
+
 
 function Sidebar() {
-  return (
-    <div className='fixed left-0 w-[3.5rem] group *:duration-500 z-20
-     justify-center items-center h-full p-1 space-y-1  *:hover:bg-zinc-100/50' dir='ltr'>
 
-        <div className='flex flex-row gap-3 bg-dark opacity-70 text-white duration-200
-                         cursor-pointer rounded-xl p-2 justify-center items-center shadow-sm'>
-         <span className='flex w-full justify-center items-center p-1'><TbMenu2 size={20} /></span>
+  const [toggle, setToggle] = useState(true);
+
+  const toggleMenu = () => {
+    setToggle(!toggle);
+  }
+
+  return (
+    <div className='fixed left-0 top-1 w-[3.5rem] z-20 overflow-hidden
+     justify-center items-center p-1 space-y-1 ' dir='ltr'>
+
+        <div onClick={toggleMenu} className='flex flex-row gap-3 bg-dark opacity-70 text-white duration-200
+                         cursor-pointer rounded-xl hover:scale-90 p-2 justify-center items-center shadow-sm'>
+         <span className='flex w-full duration-300 justify-center items-center p-1'>
+          {
+            toggle ? 
+            <TbMenu2 size={23} />
+            :
+            <TbX size={23}/>
+
+          }
+          </span>
        </div>
 
-
-       <div className='flex flex-row gap-3 w-full overflow-clip hover:w-[10rem] bg-stone-200/30 duration-200
+      <div className={`fixed space-y-2 group w-[3rem] justify-center items-center *:hover:bg-zinc-100 duration-300 ${toggle ? ' -translate-x-20 ' : 'translate-x-0'}`}>
+       <div className='flex flex-row gap-3 w-full overflow-clip hover:w-[10rem] bg-stone-200/30 duration-500
                         cursor-pointer rounded-xl p-2 justify-end items-center shadow-sm'>
          <span className=''>داشبورد </span>
          <span className='flex w-full justify-end items-center p-1'><TbHome size={22} /></span>
        </div>
                 
-        <div className='flex flex-row gap-3 w-full overflow-clip hover:w-[10rem] bg-stone-200/30 duration-200
+        <div className='flex flex-row gap-3 w-full overflow-clip hover:w-[10rem] bg-stone-200/30 duration-500
                         cursor-pointer rounded-xl p-2 justify-end items-center shadow-sm'>
          <span className=''>کاربران </span>
          <span className='flex w-full justify-end items-center p-1'><TbUserCog  size={22} className=''/></span>
          </div>
-        <div className='flex flex-row gap-3 w-full overflow-clip hover:w-[10rem] bg-stone-200/30 duration-200
+        <div className='flex flex-row gap-3 w-full overflow-clip hover:w-[10rem] bg-stone-200/30 duration-500
                         cursor-pointer rounded-xl p-2 justify-end items-center shadow-sm'>
          <span className=''>محصولات </span>
          <span className='flex w-full justify-end items-center p-1'><LiaDollyFlatbedSolid   size={24} className=''/></span>
          </div>
-        <div className='flex flex-row gap-3 w-full overflow-clip hover:w-[10rem] bg-stone-200/30 duration-200
+        <div className='flex flex-row gap-3 w-full overflow-clip hover:w-[10rem] bg-stone-200/30 duration-500
                         cursor-pointer rounded-xl p-2 justify-end items-center shadow-sm'>
          <span className=''>مقالات </span>
          <span className='flex w-full justify-end items-center p-1'><TbArticle  size={22} className=''/></span>
          </div>
-        <div className='flex flex-row gap-3 w-full overflow-clip hover:w-[10rem] bg-stone-200/30 duration-200
+        <div className='flex flex-row gap-3 w-full overflow-clip hover:w-[10rem] bg-stone-200/30 duration-500
                         cursor-pointer rounded-xl p-2 justify-end items-center shadow-sm'>
          <span className=''>دسترسی </span>
          <span className='flex w-full justify-end items-center p-1'><TbEye  size={24} className=''/></span>
          </div>
-        <div className='flex flex-row gap-3 w-full overflow-clip hover:w-[10rem] bg-stone-200/30 duration-200
+        <div className='flex flex-row gap-3 w-full overflow-clip hover:w-[10rem] bg-stone-200/30 duration-500
                         cursor-pointer rounded-xl p-2 justify-end items-center shadow-sm'>
          <span className=''>تیکت‌ </span>
          <span className='flex w-full justify-end items-center p-1'><TbMail  size={22} className=''/></span>
          </div>
-        <div className='flex flex-row gap-3 w-full overflow-clip hover:w-[10rem] bg-stone-200/30 duration-200
+        <div className='flex flex-row gap-3 w-full overflow-clip hover:w-[10rem] bg-stone-200/30 duration-500
+                        cursor-pointer rounded-xl p-2 justify-end items-center shadow-sm'>
+         <span className=''>دسته‌بندی</span>
+         <span className='flex w-full justify-end items-center p-1'><TbCategoryFilled   size={22} className=''/></span>
+         </div>
+        <div className='flex flex-row gap-3 w-full overflow-clip hover:w-[10rem] bg-stone-200/30 duration-500
                         cursor-pointer rounded-xl p-2 justify-end items-center shadow-sm'>
          <span className=''>گزارشات </span>
          <span className='flex w-full justify-end items-center p-1'><TbChartBarPopular  size={22} className=''/></span>
          </div>
+      </div>
+
+
+        
 
 
     </div>
