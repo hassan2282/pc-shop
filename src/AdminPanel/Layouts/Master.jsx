58190ea7778d94f235ccb28f60be63fd5cdc0a5c
hassan2282 <taghavey.hassan@gmail.com>
2025-10-15 02:17:50 +0330

    change background pic

--- src/AdminPanel/Layouts/Master.jsx
+++ src/AdminPanel/Layouts/Master.jsx
@@ -3,9 +3,10 @@ import Navbar from './Navbar'
 
 function Master() {
   return (
-    <div className="min-w-screen min-h-screen fixed bg-conic-180 from-indigo-600 via-pink-50 to-indigo-600">
+    // <div className="min-w-screen min-h-screen fixed bg-conic-180 from-indigo-600 via-pink-50 to-indigo-600">
+    <div className="min-w-screen min-h-screen fixed bg-[url('../../src/AdminPanel/assets/img/wallpapers/win11.jpeg')] ">
         <Navbar />
-          
+
         <Sidebar />
     </div>
   )
