4e27c2008f7ed23e012b6b0881631eb2b45a1b28
hassan2282 <taghavey.hassan@gmail.com>
2025-11-04 16:36:51 +0330

    add report link to sidebar

--- src/AdminPanel/Layouts/Sidebar.jsx
+++ src/AdminPanel/Layouts/Sidebar.jsx
@@ -85,7 +85,7 @@ function Sidebar() {
           <span className=''>نقش </span>
           <span className='flex w-full justify-end items-center p-1'><FaUnlockAlt size={20} className='' /></span>
         </Link>
-        <Link className='flex flex-row gap-3 w-full overflow-clip hover:w-[10rem] bg-[#EFF4F9]/50 duration-500
+        <Link to="/admin/report/all" className='flex flex-row gap-3 w-full overflow-clip hover:w-[10rem] bg-[#EFF4F9]/50 duration-500
                         cursor-pointer rounded-xl p-2 justify-end items-center shadow-sm'>
           <span className=''>گزارشات </span>
           <span className='flex w-full justify-end items-center p-1'><TbChartBarPopular size={22} className='' /></span>
