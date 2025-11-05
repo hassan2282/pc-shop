da064fdb32ffdb9fd88d6d2542d26304788647db
hassan2282 <taghavey.hassan@gmail.com>
2025-11-05 18:44:50 +0330

    edit motion intro in all users page

--- src/AdminPanel/Components/Users/Adm_all_users.jsx
+++ src/AdminPanel/Components/Users/Adm_all_users.jsx
@@ -1,37 +1,37 @@
 import { FaUser, FaUserSecret } from 'react-icons/fa'
 import { TbCheck, TbEditCircle, TbEyeFilled, TbSearch, TbTrashFilled, TbX } from 'react-icons/tb'
 import { Link } from 'react-router-dom'
-import {motion} from 'motion/react'
+import { motion } from 'motion/react'
 import { MdPersonAdd } from 'react-icons/md'
 import { IoMdCloseCircle } from 'react-icons/io'
 
 function Adm_all_users() {
   return (
     <motion.div
-    initial={{
-      y:20,
-      scale:0.9,
-      opacity:0
-    }}
-    animate={{
-      y:0,
-      scale:1,
-      opacity:1,
-      transition: {
-        duration: 0.8,
-      }
-    }}
-    className='flex flex-col items-center bg-white/30 backdrop-blur-xl rounded-2xl shadow-2xl border
+      initial={{
+        y: 20,
+        scale: 0.9,
+        opacity: 0
+      }}
+      animate={{
+        y: 0,
+        scale: 1,
+        opacity: 1,
+        transition: {
+          duration: 0.8,
+        }
+      }}
+      className='flex flex-col items-center bg-white/30 backdrop-blur-xl rounded-2xl shadow-2xl border
      border-white/20 mr-10 mt-4 min-h-96 w-[92%] min-w-[60rem] overflow-x-auto'>
       <div className='flex flex-row w-full justify-between items-center p-6 bg-white/30 
       backdrop-blur-md h-24 rounded-t-2xl border-b border-white/20'>
         <Link to="/admin/index" className='text-gray-600 hover:scale-120 mr-3 transition-all duration-200'>
-          <IoMdCloseCircle size={27} className='text-red-500'/>
+          <IoMdCloseCircle size={27} className='text-red-500' />
         </Link>
-          <h3 className='text-2xl font-bold text-gray-800'>مدیریت کاربران</h3>
+        <h3 className='text-2xl font-bold text-gray-800'>مدیریت کاربران</h3>
         <div className='flex flex-row relative justify-center items-center space-x-4'>
           <div className='relative flex justify-center items-center'>
-            <TbSearch size={20} className='absolute left-3 text-gray-400'/>
+            <TbSearch size={20} className='absolute left-3 text-gray-400' />
             <input
               type='search'
               className='w-[14rem] inset-shadow-sm shadow-xs hover:w-[20rem] h-12 rounded-xl p-3 bg-white/90 backdrop-blur-sm border border-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200'
@@ -41,9 +41,9 @@ function Adm_all_users() {
           <Link to="/admin/users/add" className='flex h-12 bg-blue-600 hover:bg-blue-700
           text-white items-center justify-center
            rounded-xl text-sm font-medium space-x-2 p-3 transition-all duration-200 transform hover:scale-105 shadow-lg hover:shadow-xl' >
-            <MdPersonAdd size={20}/>
+            <MdPersonAdd size={20} />
             <span>افزودن کاربر</span>
-           </Link>
+          </Link>
         </div>
       </div>
 
