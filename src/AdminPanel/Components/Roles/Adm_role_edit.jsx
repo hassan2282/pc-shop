1a8d374743fdae639c34e934d5853d0c0afe6d8c
hassan2282 <taghavey.hassan@gmail.com>
2025-12-10 01:21:55 +0330

    now Adm role edit component connected to db and tested successfully

--- src/AdminPanel/Components/Roles/Adm_role_edit.jsx
+++ src/AdminPanel/Components/Roles/Adm_role_edit.jsx
@@ -1,59 +1,106 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion } from 'motion/react'
-import { TbUser, TbBuilding, TbCheck, TbCategoryPlus } from 'react-icons/tb'
-import { Link } from 'react-router-dom'
+import { TbUser, TbCheck } from 'react-icons/tb'
+import { Link, useParams } from 'react-router-dom'
 import { IoMdCloseCircle } from 'react-icons/io'
 import { FaUnlockAlt } from 'react-icons/fa'
+import { toast } from 'react-toastify'
+import apiClient from '../../../apiClient'
 
-function Adm_role_edit() {
+function Adm_role_add() {
+  const { id } = useParams();
   const [formData, setFormData] = useState({
     name: '',
-    parent: 0,
+    permissions: [],
   })
 
   const [errors, setErrors] = useState({})
   const [isSubmitting, setIsSubmitting] = useState(false)
+  const [permissions, setPermissions] = useState([]);
 
   const validateForm = () => {
     const newErrors = {}
 
     if (!formData.name.trim()) {
-      newErrors.name = 'نام کاربری الزامی است'
+      newErrors.name = 'عنوان نقش الزامی است'
     }
 
     setErrors(newErrors)
     return Object.keys(newErrors).length === 0
   }
 
-  const handleSubmit = (e) => {
+
+  useEffect(() => {
+    const fetchRoles = async () => {
+      try {
+        const fetchResponse = await apiClient.get('/admin/permissions');
+        if (fetchResponse.status >= 200 && fetchResponse.status < 300) {
+          setPermissions(fetchResponse.data);
+        }
+        const getTargetRole = await apiClient.get(`/admin/roles/${id}`);
+        if (getTargetRole.status >= 200 && getTargetRole.status < 300) {
+          setFormData({
+            name: getTargetRole.data.name,
+            permissions: getTargetRole.data.permissions.map(item => String(item.id))
+          });
+        }
+
+      } catch (err) {
+        toast.error('خطا در فرایند واکشی داده ها');
+      }
+    }
+    fetchRoles();
+  }, []);
+
+
+  const handleSubmit = async (e) => {
     e.preventDefault()
     if (validateForm()) {
       setIsSubmitting(true)
-      // در اینجا منطق ارسال داده به سرور قرار می‌گیرد
-      console.log('Form submitted:', formData)
-      setTimeout(() => {
+      try {
+        const res = await apiClient.patch(`/admin/roles/${id}`, formData)
+        if (res.status >= 200 && res.status < 300) {
+          toast.success('نقش با موفقیت ویرایش شد');
+          setFormData({ name: '', permissions: [] });
+        }
+      } catch (err) {
+        console.log(err.response.data)
+        toast.error('خطا در فرایند ویرایش نقش ');
+
+      } finally {
         setIsSubmitting(false)
-        // نمایش پیام موفقیت
-        alert('کاربر با موفقیت ایجاد شد')
-      }, 1500)
+      }
     }
   }
 
   const handleChange = (e) => {
-    const { name, value } = e.target
-    setFormData(prev => ({
-      ...prev,
-      [name]: value
-    }))
+    const { name, value, type, checked } = e.target;
+
+    if (name === 'permissions' && type === 'checkbox') {
+      setFormData(prev => {
+        const newPermissions = checked
+          ? [...prev.permissions, value]
+          : prev.permissions?.filter(permissionId => permissionId !== value);
+        return {
+          ...prev,
+          permissions: newPermissions,
+        };
+      });
+    } else {
+      setFormData(prev => ({
+        ...prev,
+        [name]: value
+      }));
+    }
 
     // پاک کردن خطا هنگام تایپ
     if (errors[name]) {
       setErrors(prev => ({
         ...prev,
         [name]: null
-      }))
+      }));
     }
-  }
+  };
 
   return (
     <div className="min-lg:h-[80%] min-md:w-[80%] w-full mt-10 md:mr-20 min-lg:mr-30 flex items-center justify-center min-sm:p-4">
@@ -76,13 +123,13 @@ function Adm_role_edit() {
             {/* اطلاعات پایه */}
             <div className="space-y-6 w-full">
               <h2 className="min-sm:text-xl font-semibold text-gray-800 flex w-full justify-between items-center gap-2">
-                  <Link to="/admin/index" className='text-gray-600 hover:scale-120 mr-3 transition-all duration-200'>
-                    <IoMdCloseCircle size={27} className='text-red-500'/>
-                  </Link>
-                  <div className='flex flex-row gap-2'>
-                    ویرایش نقش
-                    <FaUnlockAlt size={25} className="text-blue-600 max-sm:hidden" />
-                  </div>
+                <Link to="/admin/index" className='text-gray-600 hover:scale-120 mr-3 transition-all duration-200'>
+                  <IoMdCloseCircle size={27} className='text-red-500' />
+                </Link>
+                <div className='flex flex-row gap-2'>
+                  ویرایش نقش
+                  <FaUnlockAlt size={25} className="text-blue-600 max-sm:hidden" />
+                </div>
               </h2>
 
               <div className="grid gap-6 w-full">
@@ -111,70 +158,38 @@ function Adm_role_edit() {
 
 
               <div className="relative">
-                  <label className="block text-sm font-medium text-gray-700 mb-2">
-                    دسترسی ها
-                  </label>
-                  <div className="grid max-sm:grid-cols-1 sm:grid-cols-3 min-md:grid-cols-5 gap-2 text-zinc-600 
+                <label className="block text-sm font-medium text-gray-700 mb-2">
+                  دسترسی ها
+                </label>
+                <div className="grid max-sm:grid-cols-1 sm:grid-cols-3 min-md:grid-cols-5 gap-2 text-zinc-600 
                   justify-center items-center w-full h-full
                   
                     *:flex *:w-full *:h-full *:bg-white/40 *:has-[:checked]:shadow-blue-800 *:has-[:checked]:shadow-md
                      *:has-[:checked]:text-white *:has-[:checked]:border *:has-[:checked]:border-blue-700
                     *:duration-300 *:border *:border-white/80 *:has-[:checked]:bg-blue-500/70
                     *:relative *:items-center *:justify-center *:text-center *:cursor-pointer *:rounded-xl *:p-2">
-                      <label className=''>
-                        <input className='absolute appearance-none
-                         cursor-pointer rounded-xl w-full h-full' type='checkbox' value="user"/>
-                        <span className='z-20'>کاربران</span>
-                      </label>
-                      <label className=''>
-                        <input checked className='absolute appearance-none
-                         cursor-pointer rounded-xl w-full h-full' type='checkbox' value="user"/>
-                        <span className='z-20'>تیکت ها</span>
-                      </label>
-                      <label className=''>
-                        <input checked className='absolute appearance-none
-                         cursor-pointer rounded-xl w-full h-full' type='checkbox' value="user"/>
-                        <span className='z-20'>محصولات</span>
-                      </label>
-                      <label className=''>
-                        <input className='absolute appearance-none
-                         cursor-pointer rounded-xl w-full h-full' type='checkbox' value="user"/>
-                        <span className='z-20'>مقالات</span>
-                      </label>
-                      <label className=''>
-                        <input className='absolute appearance-none
-                         cursor-pointer rounded-xl w-full h-full' type='checkbox' value="user"/>
-                        <span className='z-20'>ادمین ها</span>
-                      </label>
-                      <label className=''>
-                        <input  className='absolute appearance-none
-                         cursor-pointer rounded-xl w-full h-full' type='checkbox' value="user"/>
-                        <span className='z-20'>کنترل فروش</span>
-                      </label>
-                      <label className=''>
-                        <input className='absolute appearance-none
-                         cursor-pointer rounded-xl w-full h-full' type='checkbox' value="user"/>
-                        <span className='z-20'>تغییر ظاهر</span>
-                      </label>
-                      <label className=''>
-                        <input className='absolute appearance-none
-                         cursor-pointer rounded-xl w-full h-full' type='checkbox' value="user"/>
-                        <span className='z-20'>اپراتور پست</span>
-                      </label>
-                      <label className=''>
-                        <input  className='absolute appearance-none
-                         cursor-pointer rounded-xl w-full h-full' type='checkbox' value="user"/>
-                        <span className='z-20'>امنیت</span>
-                      </label>
-                      <label className=''>
-                        <input checked className='absolute appearance-none
-                         cursor-pointer rounded-xl w-full h-full' type='checkbox' value="user"/>
-                        <span className='z-20'>کنترل خرید</span>
-                      </label>
 
-                  </div>
+                  {
+                    permissions &&
+                    permissions.map((item) => {
+                      return (
+                        <label key={item.id} className=''>
+                          <input
+                            onChange={handleChange}
+                            name='permissions'
+                            className='absolute appearance-none cursor-pointer rounded-xl w-full h-full'
+                            type='checkbox'
+                            value={item.id}
+                            checked={formData.permissions?.includes(String(item.id))}
+                          />
+                          <span className='z-20'>{item.name}</span>
+                        </label>
+                      )
+                    })
+                  }
                 </div>
               </div>
+            </div>
 
             {/* دکمه‌های فرم */}
             <div className="flex flex-col sm:flex-row gap-4 pt-4">
@@ -183,7 +198,7 @@ function Adm_role_edit() {
                 disabled={isSubmitting}
                 className="flex-1 flex items-center justify-center gap-2 bg-blue-600 hover:bg-blue-700
                  text-white font-medium py-3 px-4 rounded-xl transition-all duration-200 transform hover:scale-105
-                  focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
+                  focus:outline-none focus:ring-2 cursor-pointer focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
               >
                 {isSubmitting ? (
                   <>
@@ -213,4 +228,4 @@ function Adm_role_edit() {
   )
 }
 
-export default Adm_role_edit
+export default Adm_role_add
