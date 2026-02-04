0b929a7d97edf9a5e5b26e708675a3dc79ef287e
hassan2282 <taghavey.hassan@gmail.com>
2026-02-05 01:01:55 +0330

    now article add work 100% successfully

--- src/AdminPanel/Components/Articles/Adm_article_add.jsx
+++ src/AdminPanel/Components/Articles/Adm_article_add.jsx
@@ -51,54 +51,29 @@ function Adm_article_add() {
     const newErrors = {}
 
     if (!formData.title.trim()) {
+      toast.error('عنوان مقاله الزامی است')
       newErrors.title = 'عنوان مقاله الزامی است'
     }
 
     if (!formData.description) {
+      toast.error('توضیح اختصاری الزامی است')
       newErrors.description = 'توضیح اختصاری الزامی است'
     } else if (formData.description.length < 20) {
+      toast.error('توضیح اختصاری باید حداقل 20 کاراکتر باشد')
       newErrors.description = 'توضیح اختصاری باید حداقل 20 کاراکتر باشد'
     }
 
     if (!formData.category_id) {
+      toast.error('دسته بندی مقاله الزامی است')
       newErrors.category_id = 'دسته بندی مقاله الزامی است'
     }
     if (!formData.media) {
+      toast.error('تصویر مقاله الزامی است')
       newErrors.media = 'تصویر مقاله الزامی است'
     }
 
     setErrors(newErrors)
     return Object.keys(newErrors).length === 0
-  }
-
-  const handleSubmit = async (e) => {
-    setIsSubmitting(true)
-    e.preventDefault()
-    if (validateForm()) {
-
-      try {
-        console.log(formData)
-        const res = apiClient.post('/admin/articles', formData);
-        if (res.status >= 200 && res.status < 300) {
-          toast.success('دسته بندی با موفقیت افزوده شد');
-          setFormData({
-            title: '',
-            description: '',
-            text: '',
-            category_id: '',
-            author_id: '',
-            media: null,
-            tags: [],
-          }
-          );
-        }
-      } catch (err) {
-        toast.error('فرایند ساخت مقاله با شکست مواجه شد !');
-      }finally{
-        setIsSubmitting(false)
-      }
-      
-    }
   }
 
   const handleChange = (e) => {
@@ -165,8 +140,48 @@ function Adm_article_add() {
     }));
   };
 
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (validateForm()) {
+      setIsSubmitting(true)
+
+      try {
+        const res = await apiClient.post('/admin/articles', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+        if (res.status >= 200 && res.status < 300) {
+          toast.success('مقاله با موفقیت افزوده شد');
+          setFormData({
+            title: '',
+            description: '',
+            text: '',
+            category_id: '',
+            author_id: '',
+            media: null,
+            tags: [],
+          },
+          );
+          setPreview('');
+          setContent('');
+        }
+      } catch (err) {
+        toast.error('فرایند ساخت مقاله با شکست مواجه شد !');
+      } finally {
+        setIsSubmitting(false)
+      }
+
+    }
+  }
+
+
+
+
   return (
-    <div className="relative items-center justify-center min-lg:w-[75%] min-lg:mr-[12%] min-sm:p-4">
+    <div className="relative items-center justify-center min-lg:w-[75%] min-lg:mr-[12%] min-sm:p-10">
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
@@ -181,7 +196,7 @@ function Adm_article_add() {
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5, delay: 0.2 }}
           className="flex w-full h-full justify-center items-center backdrop-blur-xl
-           rounded-2xl shadow-2xl border border-white/20 min-md:p-8 p-1 bg-white/10"
+           rounded-2xl shadow-2xl border border-white/20 min-md:p-8 p-1 bg-white/30"
         >
           <form onSubmit={handleSubmit} className="min-md:space-y-2 w-full space-y-2  flex flex-col">
             {/* اطلاعات پایه */}
@@ -207,9 +222,9 @@ function Adm_article_add() {
                       name="title"
                       value={formData.title}
                       onChange={handleChange}
-                      className={`w-full pl-10 pr-4 py-4 shadow-sm shadow-zinc-500 hover:shadow-md 
+                      className={`w-full pl-10 pr-4 py-4 
                         duration-200 rounded-xl border ${errors.title ? 'border-red-500' : 'border-gray-200'}
-                        focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-white/40 `}
+                        focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-white/90 `}
                       placeholder="عنوان مقاله را وارد کنید"
                     />
                   </div>
@@ -229,15 +244,15 @@ function Adm_article_add() {
                       name="category_id"
                       value={formData.category_id}
                       onChange={handleChange}
-                      className={`w-full pl-10 pr-4 py-4 shadow-sm shadow-zinc-500 hover:shadow-md duration-200 rounded-xl
+                      className={`w-full pl-10 pr-4 py-4 duration-200 rounded-xl
                          border ${errors.category_id ? 'border-red-500' : 'border-gray-200'
-                        } focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-white/40 `}
+                        } focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-white/90 `}
                       placeholder="دسته بندی">
                       {
                         categories &&
                         categories.map((item, index) => {
                           return (
-                            <option value={item.id} key={index}>{item.name}</option>
+                            <option className='text-zinc-500' value={item.id} key={index}>{item.name}</option>
                           )
                         })
                       }
@@ -251,7 +266,7 @@ function Adm_article_add() {
 
 
 
-                <div className="relative col-span-2">
+                <div className="relative col-span-3">
                   <label className="block text-sm font-medium text-gray-700 mb-2">
                     توضیح مختصر
                   </label>
@@ -261,9 +276,8 @@ function Adm_article_add() {
                       name="description"
                       value={formData.description}
                       onChange={handleChange}
-                      className={`w-full pl-10 pr-4 py-4 min-h-[12rem] h-[3.8rem] shadow-sm shadow-zinc-500
-                         hover:shadow-md duration-200 rounded-xl border ${errors.description ? 'border-red-500' : 'border-gray-200'
-                        } focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-white/40 `}
+                      className={`w-full pl-10 pr-4 py-4 min-h-[12rem] h-[3.8rem] duration-200 rounded-xl border ${errors.description ? 'border-red-500' : 'border-gray-200'
+                        } focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-white/90 `}
                       placeholder="توضیح اختصاری ..."
                     />
                   </div>
@@ -273,15 +287,15 @@ function Adm_article_add() {
                 </div>
 
 
-                <div className="relative col-span-2">
+                <div className="relative col-span-1">
                   <label className="block text-sm font-medium text-gray-700 mb-2">
                     تصویر مقاله
                   </label>
                   <div className="md:flex">
                     <div className="w-full h-full">
                       <div
-                        className="relative h-48 rounded-xl border-2 border-blue-500/20 bg-white/40 flex hover:border-blue-500/50
-                          justify-center items-center shadow-sm shadow-zinc-500 hover:shadow-md transition-shadow duration-300 ease-in-out"
+                        className="relative h-48 rounded-xl border-2 border-blue-500/20 bg-white/90 flex hover:border-blue-500/50
+                          justify-center items-center transition-shadow duration-300 ease-in-out"
                       >
                         <div className="absolute backdrop-blur-md cursor-pointer z-20 rounded-xl p-2 flex flex-col items-center">
                           <FcOldTimeCamera size={80} />
@@ -338,7 +352,7 @@ function Adm_article_add() {
                 )}
               </button>
 
-              <Link to="/admin/product/all" className='flex h-12 bg-blue-600 hover:bg-blue-700
+              <Link to="/admin/article/all" className='flex h-12 bg-blue-600 hover:bg-blue-700
                       text-white items-center justify-center
                       rounded-xl text-sm font-medium space-x-2 p-3 transition-all duration-200 
                       transform hover:scale-105 shadow-lg hover:shadow-xl' >
