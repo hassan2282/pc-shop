2535f2bbb861f7f955c709614a1c3c883b3c2b16
hassan2282 <taghavey.hassan@gmail.com>
2026-02-17 19:03:12 +0330

    add single blog ui & logic

--- src/StorePanel/components/SingleBlog.jsx
+++ src/StorePanel/components/SingleBlog.jsx
@@ -1,106 +1,144 @@
-﻿function SingleBlog() {
-  return (
-    <div>
+﻿import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import apiClient from "../../apiClient";
+import { toast } from "react-toastify";
+
+function SingleBlog() {
+
+    const BASE_URL = import.meta.env.VITE__BASEURL;
+    const { id } = useParams();
+    const [blog, setBlog] = useState();
+    const [blogWithRels, setBlogWithRels] = useState();
+
+    useEffect(() => {
+        const fetchBlog = async () => {
+            try {
+                const res = await apiClient.get(`/singleBlog/${id}`);
+                if (res.status >= 200 && res.status < 300) {
+                    setBlog(res.data[0]);
+                    setBlogWithRels(res.data[1]);
+                }
+
+            } catch (err) {
+                console.log(err?.response?.data?.message)
+                toast.error('خطا در واکشی مقاله');
+            }
+        }
+
+        fetchBlog();
+    }, []);
+
+
+    const formatDate = (date) => {
+        return new Intl.DateTimeFormat("fa-IR-u-ca-persian", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+        }).format(new Date(date));
+    };
+
+
+    return (
+        <div>
             <main className="wrapper default ">
-                    <div className="container">
-                        <div className="row">
-                    <div className="single_blog_content col-12 col-lg-9 mx-auto order-1 order-sm-1 ">
-                        <header className="card-header">
-                            <h3 className="card-title"><span> خط سفید روی نمایشگر گلکسی A71 سامسونگ؛ ماجرا چیست؟</span></h3>
-                        </header>
-                        <div className="single_blog_page">
+                <div className="container">
+                    <div className="row">
+                        <div className="single_blog_content col-12 col-lg-9 mx-auto order-1 order-sm-1 ">
+                            <header className="card-header">
+                                <h3 className="card-title"><span> {blog && blog.title} </span></h3>
+                            </header>
+                            <div className="single_blog_page">
 
-                            <div className="single_blog_box_content">
-                                <form className="form-account">
-                                    <div className="row">
+                                <div className="single_blog_box_content">
+                                    <form className="form-account">
+                                        <div className="row">
 
-                                        <div className="col-md-12 col-sm-12">
+                                            <div className="col-md-12 col-sm-12">
 
-                                            <img src="/src/StorePanel/assets/img/blog/post-2.jpg" />
-                                            <div className="data_det">
-                                                <span className="publish_date">
-                                                    <i className="fa fa-clock"></i>
-                                                    25 آذر 1402
-                                                </span>
-                                                <span className="author">
-                                                    <i className="fa fa-user-alt"></i>
-                                                    ارسال شده توسط<a href=""> مَسای</a>
-                                                </span>
-                                                <span className="categoey">
-                                                    <i className="fa fa-folder"></i>
-                                                    <a href="category-blog.html" rel="category tag">تکنولوژی</a>
-                                                </span>
-                                                <span className="visitors">
-                                                    <i className="fa fa-eye"></i> 1725 بازدید
-                                                </span>
+                                                <img src={blog && BASE_URL + "/storage/media/" + blog.media?.name} />
+                                                <div className="data_det flex flex-row justify-center items-center">
+                                                    <span className="publish_date">
+                                                        <i className="fa fa-clock"></i>
+                                                        {
+                                                          blog && formatDate(blog.created_at)
+                                                        }
+                                                    </span>
+                                                    <span className="author">
+                                                        <i className="fa fa-user-alt"></i>
+                                                        <span> {blog &&  blog.user.username} </span>
+                                                    </span>
+                                                    <span className="categoey">
+                                                        <i className="fa fa-folder"></i>
+                                                        <a href="category-blog.html" rel="category tag">{blog && blog.category?.name}</a>
+                                                    </span>
+                                                    <span className="visitors">
+                                                        <i className="fa fa-eye"></i> {blog && Math.floor(Math.random() * 10000)}
+                                                    </span>
+                                                </div>
+
+                                                <div dangerouslySetInnerHTML={{ __html: blog?.text }}></div>
+
                                             </div>
-                                            <p>
-                                                گیزموچاینا می‌نویسد مشکلی جدید در گوشی‌های هوشمند سامسونگ دیده شده که نمایشگر دستگاه را تحت تأثیر قرار می‌دهد. کاربری در توییتر از نمایش خطی سفید روی نمایشگر گلکسی A71 خبر داده است. پیش‌تر خط سبز، صورتی و ارغوانی روی نمایشگر برخی از گوشی‌های رده‌بالای سامسونگ دیده شده بود.
-                                                <br />
-
-                                                ظاهراً خط سفید به‌صورت عمودی روی نمایشگر گلکسی ای ۷۱ نقش بسته است. کاربری که نخستین‌بار مشکل را گزارش کرد، می‌گوید دستگاهش روی زمین نیفتاده و هیچ‌گونه آسیبی ندیده؛ همچنین شعبه‌ی هند حساب توییتر سامسونگ به این مشکل واکنش نشان داده است.
-                                            </p>
-                                            <p>
-                                                مشکل گلکسی A71 در حالی ظاهر می‌شود که گلکسی نوت ۲۰ اولترا نیز پس از دریافت به‌روزرسانی ماه گذشته‌ی اندروید با مشکل خط سبز و صورتی روی نمایشگر مواجه شده بود. سامسونگ در آن زمان اعلام کرد نمایشگر نوت ۲۰ اولترا را به رایگان تعویض می‌کند.
-                                            </p>
 
                                         </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
 
+                        <div className="col-12 col-lg-3">
 
-
+                            <div className="sidebar_blog">
+                                <div className="widget_blog">
+                                    <div className="widget_blog_headbox">
+                                        <h3> دسته بندی ها </h3>
                                     </div>
-                                </form>
+                                    <div className="blog_list_widget_blog">
+                                        <div className="widget_blog_groups">
+                                            <ul className="widget_blog_posts">
+                                                {
+                                                    blogWithRels &&
+                                                    blogWithRels.map((item, index) => {
+                                                        return (
+
+                                                            <li key={index}><a className="" href=""> {item.category.name} </a></li>
+                                                            
+                                                        )        
+                                                    })
+                                                }
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="widget_blog marg_top20">
+                                    <div className="widget_blog_headbox">
+                                        <h3>آخرین مقالات </h3>
+                                    </div>
+                                    <div className="blog_list_widget_blog">
+                                        <div className="widget_blog_groups">
+                                            <ul className="widget_blog_posts">
+                                                {
+                                                    blogWithRels &&
+                                                    blogWithRels.map((item, index) => {
+                                                        return (
+                                                            <li key={index}><a className="widget_blog_title_link" href="">{item.description.slice(0,45) + ' ...'}</a></li>
+                                                        )        
+                                                    })
+                                                }
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                     </div>
-
-                    <div className="col-12 col-lg-3">
-
-                        <div className="sidebar_blog">
-                            <div className="widget_blog">
-                                <div className="widget_blog_headbox">
-                                    <h3> دسته بندی ها </h3>
-                                </div>
-                                <div className="blog_list_widget_blog">
-                                    <div className="widget_blog_groups">
-                                        <ul className="widget_blog_posts">
-                                            <li><a className="widget_blog_title_link" href=""> تکنولوژی آینده </a></li>
-                                            <li><a className="widget_blog_title_link" href=""> پرچم دار موبایل </a></li>
-                                            <li><a className="widget_blog_title_link" href=""> ویدئو های کاربردی </a></li>
-                                            <li><a className="widget_blog_title_link" href=""> ترفند های آموزشی </a></li>
-                                            <li><a className="widget_blog_title_link" href=""> دانلود نرم افزار </a></li>
-                                            <li><a className="widget_blog_title_link" href=""> دنیای رمز ارز </a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="widget_blog marg_top20">
-                                <div className="widget_blog_headbox">
-                                    <h3>آخرین مقالات </h3>
-                                </div>
-                                <div className="blog_list_widget_blog">
-                                    <div className="widget_blog_groups">
-                                        <ul className="widget_blog_posts">
-                                            <li><a className="widget_blog_title_link" href=""> این‌ بار هوش مصنوعی، تصاویری از کارخانه تولید عروس دریایی را افشا می‌کند!</a></li>
-                                            <li><a className="widget_blog_title_link" href="">  خط سفید روی نمایشگر گلکسی A71 سامسونگ؛ ماجرا چیست؟</a></li>
-                                            <li><a className="widget_blog_title_link" href="">بهترین گوشی های مناسب عکاسی در بازار ایران</a></li>
-                                            <li><a className="widget_blog_title_link" href="">هزینه ساخت بازی‌های انحصاری پلی استیشن با فیلم‌های بلاک‌باستر برابری می‌کند</a></li>
-                                            <li><a className="widget_blog_title_link" href=""> یک دهه سکوت شکست؛ مایکروسافت آمار فروش ایکس باکس را فاش کرد!</a></li>
-                                            <li><a className="widget_blog_title_link" href="">  بررسی تلویزیون 4K اسنوا؛ ۵۵ اینچی اقتصادی برای فیلم‌بازها!</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                    </div>
-                    </div>
+                </div>
             </main>
-                
 
-    </div>
-  )
+
+        </div>
+    )
 }
 
 export default SingleBlog
