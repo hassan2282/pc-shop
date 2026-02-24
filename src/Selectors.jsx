6a8c1599bca6cc499789f0fce64794047725c63e
hassan2282 <taghavey.hassan@gmail.com>
2026-02-24 16:31:22 +0330

    debug total price in selector

--- src/Selectors.jsx
+++ src/Selectors.jsx
@@ -1,2 +1,2 @@
 export const selectTotalPrice = (state) => 
-    state.cart.reduce((acc, item) => acc + (item.count + item.price),0);
+    state.cart.reduce((acc, item) => acc + (item.count * item.price),0);
