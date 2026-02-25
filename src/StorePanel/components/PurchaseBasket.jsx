1f413c264ba70ecc60945d857fa964204b7bb3fe
hassan2282 <taghavey.hassan@gmail.com>
2026-02-25 08:35:19 +0330

    purchaseBasket completed

--- src/StorePanel/components/PurchaseBasket.jsx
+++ src/StorePanel/components/PurchaseBasket.jsx
@@ -1,33 +1,44 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { selectTotalPrice } from '../../Selectors'
+import { motion } from 'motion/react'
 
-function PurchaseBasket() {
+function PurchaseBasket({ toggle, setToggle }) {
     const BASE_URL = import.meta.env.VITE__BASEURL;
     const cart = useSelector(state => state.cart);
+    const dispatch = useDispatch();
+    const totalPrice = useSelector(selectTotalPrice);
 
-    const deleteHandler = (id) => {
-        alert('hi')
+    const deleteHandler = (index) => {
+        const updatedCart = cart.filter((_, i) => i !== index);
+        dispatch({ type: "deleteProduct", payload: updatedCart });
+        localStorage.setItem("cart", JSON.stringify(updatedCart));
     }
 
     return (
         <>
-            <ul style={{height: '350px'}} className="rounded-xl overflow-y-scroll p-2">
+            <ul style={{"maxHeight": '22rem'}} className="rounded-xl overflow-y-scroll p-2 overflow-clip">
                 {
-                    cart &&
+                    cart.length > 0 ?
                     cart.map((item, index) => {
                         return (
-                            <li key={index} className="flex justify-center items-center rounded-xl relative bg-[#F1F9FA] border-b border-white h-20" dir='rtl'>
-                                <Link to={`/store/single-product/${item.id}`} className="d-flex flex-row">
-                                    <i onClick={()=>deleteHandler(item.id)} className="absolute top-3 right-2
-                                     fa fa-times text-red-500 rounded-full hover:scale-110" aria-hidden="true"></i>
+                            <motion.li
+                                initial={{ x: 100, opacity: 0 }}
+                                animate={{ x: 0, opacity: 1 }}
+                                exit={{ x: 100, opacity: 0 }}
+                                transition={{ delay: 0.2 * index, duration: 0.5, ease: "easeInOut" }}
+                                key={index} className="flex justify-center items-center rounded-xl relative
+                                                     bg-[#F1F9FA] border-b border-white h-20" dir='rtl'>
+                                <a href={`/store/single-product/${item.id}`} onClick={() => setToggle(!toggle)} className="d-flex flex-row">
+                                    <i onClick={() => deleteHandler(index)} className="absolute top-3 right-2
+                                     fa fa-times text-red-500 hover:text-red-800 rounded-full hover:scale-110" aria-hidden="true"></i>
                                     <div className="d-flex flex-row justify-center items-center p-1">
-                                        <div className="m_cart-item-image w-[20%]">
+                                        <div className="w-[20%]">
                                             <img src={`${BASE_URL}/storage/media/${item.media}`} />
                                         </div>
-                                        <div className="m_cart-item-details">
+                                        <div className="m_cart-item-details mr-4">
                                             <div className="m_cart-item-title">
-                                                {item.title}
+                                                {item.title?.slice(0, 15) + ' ...'}
                                             </div>
                                             <div className="">
                                                 <div className="space-x-4 text-muted text-[12px]">
@@ -37,23 +48,25 @@ function PurchaseBasket() {
                                             </div>
                                         </div>
                                     </div>
-                                </Link>
-                            </li>
+                                </a>
+                            </motion.li>
                         )
                     })
+                    :
+                    <span className='text-danger'>محصولی به سبد خرید اضافه نشده</span>
                 }
 
             </ul>
             <div className="m_cart-header">
-                <div className="m_cart-total p-2">
+                <div className="p-4">
                     <span>مجموع سبد:</span>
-                    <span> ۳۵,۲۵۳,۵۰۰</span>
+                    <span>{cart.length > 0 ? totalPrice.toLocaleString('en-US') : 0}</span>
                     <span> تومان</span>
                 </div>
             </div>
             <div className="btn_cart">
-                <Link to="/store/cart" className="btn btn_sabad">مشاهده سبد</Link>
-                <Link to="/store/final-payment" className="btn btn_pardakht btn-main-masai">پرداخت</Link>
+                <Link to="/store/cart" onClick={() => setToggle(!toggle)} className="btn btn_sabad">مشاهده سبد</Link>
+                <Link to="/store/final-payment" onClick={() => setToggle(!toggle)} className="btn btn_pardakht btn-main-masai">پرداخت</Link>
             </div>
         </>
     )
