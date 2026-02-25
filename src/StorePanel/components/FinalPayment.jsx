dc8ca7d34532aeaaf7f4501ea8724cd5aa0a00b4
hassan2282 <taghavey.hassan@gmail.com>
2026-02-25 08:34:04 +0330

    start to connect finalpayment to db

--- src/StorePanel/components/FinalPayment.jsx
+++ src/StorePanel/components/FinalPayment.jsx
@@ -1,140 +1,159 @@
-﻿function FinalPayment() {
-  return (
-    <div>
+﻿import { useState } from "react";
+import { useSelector } from "react-redux"
+import { Link } from "react-router-dom"
 
-        <main className="wrapper default">
+function FinalPayment() {
+    const user = useSelector(state => state.user);
+    const [formData, setFormData] = useState({
+        first_name: user?.first_name || '',
+        last_name: user?.last_name || '',
+        phone: user?.phone || '',
+        email: user?.email || '',
+        province_id: '',
+        city_id: '',
+        postal_code: '',
+        address: '',
+    });
 
-        <main className="cart-page default ">
-            <div className="container">
-                <div className="row">
-                    <div className="Final_payment_content col-12 mx-auto">
-                        <header className="card-header">
-                            <h3 className="card-title"><span>تسویه حساب</span></h3>
-                        </header>
-                        <div className="account-box Final_payment_page">
+    console.log(formData)
 
-                            <div className="account-box-content">
-                                <form className="form-account">
-                                    <div className="row">
-                                        <div className="col-md-8 col-sm-12">
+
+    return (
+        <div>
+
+            <main className="wrapper default">
+
+                <main className="cart-page default ">
+                    <div className="container">
+                        <div className="row">
+                            <div className="Final_payment_content col-12 mx-auto">
+                                <header className="card-header">
+                                    <h3 className="card-title"><span>افزودن و تایید اطلاعات</span></h3>
+                                </header>
+                                <div className="account-box Final_payment_page">
+
+                                    <div className="account-box-content">
+                                        <form className="form-account">
                                             <div className="row">
-                                                <div className="col-md-6 col-sm-12">
-                                                    <div className="form-account-title"><span>*</span> نام</div>
-                                                    <div className="form-account-row">
-                                                        <input className="input_second input_all" type="text" placeholder=" نام شما"/>
-                                                    </div>
-                                                </div>
-                                                <div className="col-md-6 col-sm-12">
-                                                    <div className="form-account-title"><span>*</span> نام خانوادگی</div>
-                                                    <div className="form-account-row">
-                                                        <input className="input_second input_all" type="text" placeholder=" نام خانوادگی شما"/>
-                                                    </div>
-                                                </div>
-                                                <div className="col-md-12 col-sm-12">
-                                                    <div className="form-account-title"> نام شرکت (اختیاری)</div>
-                                                    <div className="form-account-row">
-                                                        <input className="input_second input_all" type="text"
-                                                               placeholder=" نام شرکت شما"/>
-                                                    </div>
-                                                </div>
-                                                <div className="col-md-6 col-sm-12">
-                                                    <div className="form-account-title"><span>*</span> شماره تماس</div>
-                                                    <div className="form-account-row">
-                                                        <input className="input_second input_all" type="text"
-                                                               placeholder=" شماره تماس شما"/>
-                                                    </div>
-                                                </div>
-                                                <div className="col-md-6 col-sm-12">
-                                                    <div className="form-account-title"><span>*</span> پست الکترونیک</div>
-                                                    <div className="form-account-row">
-                                                        <input className="input_second input_all" type="text"
-                                                               placeholder=" پست الکترونیک شما"/>
-                                                    </div>
-                                                </div>
-                                                <div className="col-12">
-                                                    <div className="form-account-title">آدرس پستی</div>
-                                                    <div className="form-account-row">
-                                                        <textarea className="input_second input_all input_textarea text-right" rows="5"
-                                                                  placeholder=" شماره موبایل خود را وارد نمایید"></textarea>
-                                                    </div>
-                                                </div>
-                                                <div className="col-12">
-                                                    <div className="form-account-agree">
-                                                        <label className="checkbox-form checkbox-primary">
-                                                            <input type="checkbox" id="agree"/>
-                                                            <span className="checkbox-check"></span>
-                                                        </label>
-                                                        <label htmlFor="agree"> گیرنده خودم هستم</label>
-                                                    </div>
-                                                </div>
+                                                <div className="col-md-8 col-sm-12">
+                                                    <div className="row">
+                                                        <div className="col-md-6 col-sm-12">
+                                                            <div className="form-account-title"><span>*</span> نام</div>
+                                                            <div className="form-account-row">
+                                                                <input className="input_second input_all" type="text" placeholder=" نام شما" />
+                                                            </div>
+                                                        </div>
+                                                        <div className="col-md-6 col-sm-12">
+                                                            <div className="form-account-title"><span>*</span> نام خانوادگی</div>
+                                                            <div className="form-account-row">
+                                                                <input className="input_second input_all" type="text" placeholder=" نام خانوادگی شما" />
+                                                            </div>
+                                                        </div>
+                                                        <div className="col-md-12 col-sm-12">
+                                                            <div className="form-account-title"> نام شرکت (اختیاری)</div>
+                                                            <div className="form-account-row">
+                                                                <input className="input_second input_all" type="text"
+                                                                    placeholder=" نام شرکت شما" />
+                                                            </div>
+                                                        </div>
+                                                        <div className="col-md-6 col-sm-12">
+                                                            <div className="form-account-title"><span>*</span> شماره تماس</div>
+                                                            <div className="form-account-row">
+                                                                <input className="input_second input_all" type="text"
+                                                                    placeholder=" شماره تماس شما" />
+                                                            </div>
+                                                        </div>
+                                                        <div className="col-md-6 col-sm-12">
+                                                            <div className="form-account-title"><span>*</span> پست الکترونیک</div>
+                                                            <div className="form-account-row">
+                                                                <input className="input_second input_all" type="text"
+                                                                    placeholder=" پست الکترونیک شما" />
+                                                            </div>
+                                                        </div>
+                                                        <div className="col-12">
+                                                            <div className="form-account-title">آدرس پستی</div>
+                                                            <div className="form-account-row">
+                                                                <textarea className="input_second input_all input_textarea text-right" rows="5"
+                                                                    placeholder=" شماره موبایل خود را وارد نمایید"></textarea>
+                                                            </div>
+                                                        </div>
+                                                        <div className="col-12">
+                                                            <div className="form-account-agree">
+                                                                <label className="checkbox-form checkbox-primary">
+                                                                    <input type="checkbox" id="agree" />
+                                                                    <span className="checkbox-check"></span>
+                                                                </label>
+                                                                <label htmlFor="agree"> گیرنده خودم هستم</label>
+                                                            </div>
+                                                        </div>
 
 
+                                                    </div>
+                                                </div>
+                                                <div className="col-md-4 col-sm-12">
+                                                    <div className="row">
+
+                                                        <table className="table table_details table-bordered">
+                                                            <thead>
+                                                                <tr>
+                                                                    <th>محصول</th>
+                                                                    <th>قیمت کل</th>
+
+                                                                </tr>
+                                                            </thead>
+                                                            <tbody>
+                                                                <tr>
+                                                                    <td>شیائومی مدل Poco X4</td>
+                                                                    <td>5,398,000 <span>تومان</span></td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td> ساعت هوشمند امیزفیت</td>
+                                                                    <td>5,398,000 <span>تومان</span></td>
+                                                                </tr>
+                                                                <tr className="all">
+                                                                    <td>مجموع	</td>
+                                                                    <td>10,798,000 <span>تومان</span></td>
+                                                                </tr>
+
+                                                                <tr>
+                                                                    <td colspan="2" className="Final_payment_det">
+
+                                                                        <div className="col-12 ">
+                                                                            <p>
+                                                                                <i className="fa fa-circle"></i> بعد از پرداخت مستقیم به شماره حساب شرکت، از قسمت چت آنلاین سایت رسید را برای ما ارسال کرده تا پس از تایید محصول برای شما ارسال گردد.
+                                                                                <br />
+                                                                                <i className="fa fa-circle"></i> برای حفظ محیط زیست از نسخه الکترونیکی فاکتور در پروفایل خود به جای چاپ کاغذی می توانید استفاده کنید.
+                                                                            </p>
+                                                                        </div>
+
+                                                                    </td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td colspan="2"><Link to="/store/shopping-payment" className="btn big_btn btn-main-masai">انتخاب شیوه پرداخت </Link></td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+
+
+                                                    </div>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className="col-md-4 col-sm-12">
-                                            <div className="row">
-
-                                                <table className="table table_details table-bordered">
-                                                    <thead>
-                                                        <tr>
-                                                            <th>محصول</th>
-                                                            <th>قیمت کل</th>
-
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        <tr>
-                                                            <td>شیائومی مدل Poco X4</td>
-                                                            <td>5,398,000 <span>تومان</span></td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td> ساعت هوشمند امیزفیت</td>
-                                                            <td>5,398,000 <span>تومان</span></td>
-                                                        </tr>
-                                                        <tr className="all">
-                                                            <td>مجموع	</td>
-                                                            <td>10,798,000 <span>تومان</span></td>
-                                                        </tr>
-
-                                                        <tr>
-                                                            <td colspan="2" className="Final_payment_det">
-
-                                                                <div className="col-12 ">
-                                                                    <p>
-                                                                        <i className="fa fa-circle"></i> بعد از پرداخت مستقیم به شماره حساب شرکت، از قسمت چت آنلاین سایت رسید را برای ما ارسال کرده تا پس از تایید محصول برای شما ارسال گردد.
-                                                                        <br />
-                                                                        <i className="fa fa-circle"></i> برای حفظ محیط زیست از نسخه الکترونیکی فاکتور در پروفایل خود به جای چاپ کاغذی می توانید استفاده کنید.
-                                                                    </p>
-                                                                </div>
-
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td colspan="2"><a href="#" className="btn big_btn btn-main-masai">ثبت سفارش </a></td>
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
 
 
-                                            </div>
-                                        </div>
+
+                                        </form>
                                     </div>
-
-
-
-                                </form>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
 
-        </main>
+                </main>
 
-        </main>
+            </main>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default FinalPayment
