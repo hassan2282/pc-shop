cffb1d89babdcdfd04656661a00555b93f1fb042
hassan2282 <taghavey.hassan@gmail.com>
2026-02-25 08:32:40 +0330

    debug address.jsx

--- src/StorePanel/components/Address.jsx
+++ src/StorePanel/components/Address.jsx
@@ -2,24 +2,26 @@
 import OrderSideBar from "../structure/OrderSideBar";
 import apiClient from "../../apiClient";
 import { toast } from "react-toastify";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { AiOutlineLoading } from "react-icons/ai";
 import { Link, useNavigate } from "react-router-dom";
-import {motion} from "motion/react";
+import { motion } from "motion/react";
 
 function Address() {
   const [provinces, setProvinces] = useState();
   const [cities, setCities] = useState();
   const [isLoading, setIsLoading] = useState(false);
   const user = useSelector((state) => state.user);
+  const clientAddress = useSelector((state) => state.address);
   const [errors, setErrors] = useState({});
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const [userAddress, setUserAddress] = useState({
-    province_id: "",
-    city_id: "",
-    postal_code: "",
-    address: "",
+    province_id: clientAddress?.provice_id || "",
+    city_id: clientAddress?.city_id || "",
+    postal_code: clientAddress?.postal_code || "",
+    address: clientAddress?.address || "",
     user_id: user?.id,
   });
 
@@ -51,26 +53,54 @@ function Address() {
     const { name, value } = e.target;
     setUserAddress((prev) => ({ ...prev, [name]: value }));
   };
-  
+
 
   const SubmitHandler = async (e) => {
     e.preventDefault();
     setIsLoading(true);
     setErrors({});
     try {
-      const res = await apiClient.post("address", userAddress );
+      const res = await apiClient.post("address", userAddress);
       if (res.status >= 200 && res.status < 300) {
-        navigate('/store/order-address', {replace: true})
+        dispatch({
+          type: "ADD_ADDRESS",
+          payload: res.data
+        })
+        localStorage.setItem("address", JSON.stringify(res.data));
+        navigate('/store/order-address', { replace: true })
         toast.success("فرایند ثبت آدرس با موفقیت انجام شد");
       }
     } catch (err) {
-      setErrors(err.response.data);
-      toast.error(err.response.data.message)
+      setErrors(err.response?.data);
+      toast.error(err.response?.data?.message)
       toast.error("شما فقط یک آدرس می توانید ثبت کنید");
     } finally {
       setIsLoading(false);
     }
   };
+
+
+
+  const deleteAddress = async () => {
+    try {
+      const res = await apiClient.delete(`delete-address/${clientAddress?.id}`)
+      if (res.status >= 200 && res.status < 300)
+        setUserAddress({
+          province_id: "",
+          city_id: "",
+          postal_code: "",
+          address: "",
+          user_id: user?.id,
+        });
+      localStorage.removeItem("address");
+      dispatch({ type: "DELETE_ADDRESS" })
+      toast.success('آدرس با موفقیت حذف گردید')
+    } catch (err) {
+      toast.error('متاسفانه در فرایند حذف مشکلی بوجود آمده است');
+    }
+  }
+
+  console.log(clientAddress)
 
   return (
     <div>
@@ -91,20 +121,20 @@ function Address() {
                   <div className="flex content-section default w-full justify-start min-sm:pr-6 items-center">
                     <form onSubmit={SubmitHandler}>
                       <p className="flex w-full">
-                        لطفا آدرس محل (سکونت / کار) خود را با دقت وارد نمایید (توجه : شما فقط مجاز به ثبت یک آدرس هستید)
+                        لطفا آدرس محل (سکونت / کار) خود را با دقت وارد نمایید <span className="text-danger">(توجه : شما فقط مجاز به ثبت یک آدرس هستید)</span>
                       </p>
 
                       <div className="row flex justify-center items-center">
                         <div className="max-sm:flex max-sm:flex-col min-sm:grid min-sm:grid-rows-3 min-sm:grid-cols-3
                                         justify-center items-center gap-3 max-sm:w-full my-3 overflow-clip">
-                          <motion.div 
+                          <motion.div
                             initial={{
-                                y: -30,
-                                filter: "blur(10px)",
+                              y: -30,
+                              opacity: 0,
                             }}
                             animate={{
                               y: 0,
-                              filter: "blur(0)",
+                              opacity: 1,
                               transition: {
                                 delay: 0.3,
                                 duration: 1,
@@ -124,6 +154,7 @@ function Address() {
                                 required
                                 placeholder="استان"
                               >
+                                <option value="">انتخاب استان</option>
                                 {provinces &&
                                   provinces.map((item) => {
                                     return (
@@ -135,20 +166,20 @@ function Address() {
                               </select>
                             </label>
                           </motion.div>
-                          <motion.div 
+                          <motion.div
                             initial={{
-                                y: -30,
-                                filter: "blur(10px)",
+                              y: -30,
+                              opacity: 0,
                             }}
                             animate={{
                               y: 0,
-                              filter: "blur(0)",
+                              opacity: 1,
                               transition: {
-                                delay: 0.4,
+                                delay: 0.6,
                                 duration: 1,
                               }
                             }}
-                           className="flex w-full">
+                            className="flex w-full">
                             <label className="flex w-full">
                               <select
                                 className={`${errors?.errors?.city_id ? 'border-red-500 bg-red-300 border animate-pulse' : 'border-[#c0e5e7] bg-[#eaf6f7] border'}  p-2 w-full rounded-full`}
@@ -156,8 +187,10 @@ function Address() {
                                 type="number"
                                 onChange={changeHandler}
                                 required
+                                defaultValue={userAddress?.city_id || ''}
                                 placeholder="شهر ها"
                               >
+                                <option value="">انتخاب شهر</option>
                                 {cities &&
                                   cities.map((item) => {
                                     return (
@@ -170,16 +203,16 @@ function Address() {
                             </label>
                           </motion.div>
 
-                          <motion.div 
+                          <motion.div
                             initial={{
-                                y: -30,
-                                filter: "blur(10px)",
+                              y: -30,
+                              opacity: 0,
                             }}
                             animate={{
                               y: 0,
-                              filter: "blur(0)",
+                              opacity: 1,
                               transition: {
-                                delay: 0.4,
+                                delay: 0.9,
                                 duration: 1,
                               }
                             }} className="flex w-full">
@@ -188,51 +221,53 @@ function Address() {
                                 className={`${errors?.errors?.postal_code ? 'border-red-500 bg-red-300 border animate-pulse' : 'border-[#c0e5e7] bg-[#eaf6f7] border'}  p-2 w-full rounded-full`}
                                 name="postal_code"
                                 type="number"
+                                value={userAddress?.postal_code}
                                 onChange={changeHandler}
                                 required
                                 placeholder="کد پستی"
                               />
                             </label>
                           </motion.div>
-                          <motion.div 
+                          <motion.div
                             initial={{
-                                y: -30,
-                                filter: "blur(10px)",
+                              y: -30,
+                              opacity: 0,
                             }}
                             animate={{
                               y: 0,
-                              filter: "blur(0)",
+                              opacity: 1,
                               transition: {
-                                delay: 0.5,
+                                delay: 1.1,
                                 duration: 1,
                               }
                             }} className="max-sm:w-full max-sm:flex min-sm:row-span-2 h-full min-sm:col-span-3">
                             <label className='flex w-full h-full'>
                               <textarea
                                 onChange={changeHandler}
-                                className={`${errors?.errors?.address ? 'border-red-500 bg-red-300 border animate-pulse' 
+                                className={`${errors?.errors?.address ? 'border-red-500 bg-red-300 border animate-pulse'
                                   : 'border-[#c0e5e7] bg-[#eaf6f7] border'}  p-2 w-full rounded-3xl h-full`}
                                 name="address"
+                                value={userAddress?.address}
                                 placeholder="آدرس دقیق : "
                               />
                             </label>
                           </motion.div>
-                          <motion.div 
+                          <motion.div
                             initial={{
-                                y: -30,
-                                filter: "blur(10px)",
+                              y: -30,
+                              opacity: 0,
                             }}
                             animate={{
                               y: 0,
-                              filter: "blur(0)",
+                              opacity: 1,
                               transition: {
-                                delay: 0.6,
+                                delay: 1.4,
                                 duration: 1,
                               }
                             }} className="flex justify-center items-center w-full">
                             <button
                               type="submit"
-                              className="btn big_btn btn-main-masai col-12"
+                              className={`btn big_btn btn-main-masai ${clientAddress?.address ? 'col-6' : 'col-12'} `}
                             >
                               {isLoading ? (
                                 <AiOutlineLoading
@@ -243,6 +278,15 @@ function Address() {
                                 "ثبت"
                               )}
                             </button>
+                            {
+                              clientAddress?.address &&
+                              <button
+                                onClick={deleteAddress}
+                                className="btn big_btn btn-danger col-6"
+                              >
+                                حذف آدرس
+                              </button>
+                            }
                           </motion.div>
 
                         </div>
