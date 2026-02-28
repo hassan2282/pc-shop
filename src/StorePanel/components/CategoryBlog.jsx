8d95ffdc5a431920ac16a3a3f658eea3218bca48
hassan2282 <taghavey.hassan@gmail.com>
2026-02-28 20:49:28 +0330

    connect category blog to db

--- src/StorePanel/components/CategoryBlog.jsx
+++ src/StorePanel/components/CategoryBlog.jsx
@@ -1,219 +1,112 @@
-﻿import Footer from "../structure/Footer"
-import Header from "../structure/Header"
+﻿import { useEffect, useState } from "react";
+import apiClient from "../../apiClient";
+import BlogSidebar from "./BlogSidebar"
+import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
 
 function CategoryBlog() {
-  return (
-    <div>
-      <main className="category-blog default space-top-30">
-            <div className="container ">
-               
-                <div className="row">
-                    <div className="col-12">
-                        <nav>
-                            <ul className="breadcrumb">
-                                <li>
-                                    <i className="fa fa-home" aria-hidden="true"></i>
-                                </li>
 
-                                <li>
-                                    <a href="#"><span>بلاگ</span></a>
-                                </li>
-                                <li>
-                                    <a href="category-blog.html"><span>اخبار روز تکنولوژی</span></a>
-                                </li>
-                              
-                            </ul>
-                        </nav>
-                    </div>
-                    <div className="single_blog_content cat_blog_content col-12 col-lg-9 mx-auto order-1 order-sm-1 ">
+    const BASE_URL = import.meta.env.VITE__BASEURL;
+    const [blogs, setBlogs] = useState();
 
-                        <div className="row listing-items Blog-category">
-                            <div className="col-xl-6 col-lg-6 col-md-6 col-12  ">
-                                <div className="blog_tag">
-                                    <a href="#">
-                                        <img src="src/StorePanel/assets/img/blog/post-1.jpg" className="img-fluid" alt=""/>
-                                    </a>
-                                    <a href="#">
-                                        <h2 className="Blog_title">
-                                            این‌ بار هوش مصنوعی، تصاویری از کارخانه تولید عروس دریایی را افشا می‌کند!
-                                        </h2>
-                                    </a>
-                                    <div className="Blog_list">
-                                        <span className="Blog_author">
-                                            <i className="fa fa-user"></i>
-                                            رضایی
-                                        </span>
-                                        <span className="Blog_Date">
-                                            <i className="fa fa-calendar "></i>
-                                            ۱4 اسفند 1402
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-6 col-lg-6 col-md-6 col-12  ">
-                                <div className="blog_tag">
-                                    <a href="#">
-                                        <img src="src/StorePanel/assets/img/blog/post-2.jpg" className="img-fluid" alt=""/>
-                                    </a>
-                                    <a href="#">
-                                        <h2 className="Blog_title">
-                                            خط سفید روی نمایشگر گلکسی A71 سامسونگ؛ ماجرا چیست؟
-                                        </h2>
-                                    </a>
-                                    <div className="Blog_list">
-                                        <span className="Blog_author">
-                                            <i className="fa fa-user"></i>
-                                            صادقی
-                                        </span>
-                                        <span className="Blog_Date">
-                                            <i className="fa fa-calendar "></i>
-                                            ۱4 اسفند 1402
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-6 col-lg-6 col-md-6 col-12  ">
-                                <div className="blog_tag">
-                                    <a href="#">
-                                        <img src="src/StorePanel/assets/img/blog/post-3.jpg" className="img-fluid" alt=""/>
-                                    </a>
-                                    <a href="#">
-                                        <h2 className="Blog_title">
-                                            بهترین گوشی های مناسب عکاسی در بازار ایران
-                                        </h2>
-                                    </a>
-                                    <div className="Blog_list">
-                                        <span className="Blog_author">
-                                            <i className="fa fa-user"></i>
-                                            صادقی
-                                        </span>
-                                        <span className="Blog_Date">
-                                            <i className="fa fa-calendar "></i>
-                                            ۱4 اسفند 1402
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-6 col-lg-6 col-md-6 col-12  ">
-                                <div className="blog_tag">
-                                    <a href="#">
-                                        <img src="src/StorePanel/assets/img/blog/post-4.jpg" className="img-fluid" alt=""/>
-                                    </a>
-                                    <a href="#">
-                                        <h2 className="Blog_title">
-                                          هزینه ساخت بازی‌های انحصاری پلی استیشن با فیلم‌های بلاک‌باستر برابری می‌کند
-                                        </h2>
-                                    </a>
-                                    <div className="Blog_list">
-                                        <span className="Blog_author">
-                                            <i className="fa fa-user"></i>
-                                            صادقی
-                                        </span>
-                                        <span className="Blog_Date">
-                                            <i className="fa fa-calendar "></i>
-                                            ۱4 اسفند 1402
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                            
-                            <div className="col-xl-6 col-lg-6 col-md-6 col-12  ">
-                                <div className="blog_tag">
-                                    <a href="#">
-                                        <img src="src/StorePanel/assets/img/blog/post-5.jpg" className="img-fluid" alt=""/>
-                                    </a>
-                                    <a href="#">
-                                        <h2 className="Blog_title">
-                                            یک دهه سکوت شکست؛ مایکروسافت آمار فروش ایکس باکس را فاش کرد
-                                        </h2>
-                                    </a>
-                                    <div className="Blog_list">
-                                        <span className="Blog_author">
-                                            <i className="fa fa-user"></i>
-                                            صادقی
-                                        </span>
-                                        <span className="Blog_Date">
-                                            <i className="fa fa-calendar "></i>
-                                            ۱4 اسفند 1402
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-6 col-lg-6 col-md-6 col-12  ">
-                                <div className="blog_tag">
-                                    <a href="#">
-                                        <img src="src/StorePanel/assets/img/blog/post-6.jpg" className="img-fluid" alt=""/>
-                                    </a>
-                                    <a href="#">
-                                        <h2 className="Blog_title">
-                                            بررسی تلویزیون 4K اسنوا؛ ۵۵ اینچی اقتصادی برای فیلم‌بازها!
-                                        </h2>
-                                    </a>
-                                    <div className="Blog_list">
-                                        <span className="Blog_author">
-                                            <i className="fa fa-user"></i>
-                                            صادقی
-                                        </span>
-                                        <span className="Blog_Date">
-                                            <i className="fa fa-calendar "></i>
-                                            ۱4 اسفند 1402
-                                        </span>
-                                    </div>
-                                </div>
+    useEffect(() => {
+        const fetchBlog = async () => {
+            try {
+                const res = await apiClient.get('/allBlogs');
+                if (res.status >= 200 && res.status < 300) {
+                    setBlogs(res.data);
+                }
+
+            } catch (err) {
+                toast.error('خطا در واکشی مقالات');
+            }
+        }
+
+        fetchBlog();
+    }, []);
+
+
+    const formatDate = (date) => {
+        return new Intl.DateTimeFormat("fa-IR-u-ca-persian", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+        }).format(new Date(date));
+    };
+
+
+
+    return (
+        <div>
+            <main className="category-blog default space-top-30">
+                <div className="container ">
+
+                    <div className="row">
+                        <div className="col-12">
+                            <nav>
+                                <ul className="breadcrumb">
+                                    <li>
+                                        <i className="fa fa-home" aria-hidden="true"></i>
+                                    </li>
+
+                                    <li>
+                                        <a href="#"><span>بلاگ</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="category-blog.html"><span>اخبار روز تکنولوژی</span></a>
+                                    </li>
+
+                                </ul>
+                            </nav>
+                        </div>
+                        <div className="single_blog_content cat_blog_content col-12 col-lg-9 mx-auto order-1 order-sm-1 ">
+
+                            <div className="row listing-items Blog-category">
+                                {
+                                    blogs &&
+                                    blogs.map((blog, index) => {
+                                        return (
+                                            <div className="col-xl-6 col-lg-6 col-md-6 col-12  ">
+                                                <div className="blog_tag">
+                                                    <Link to={`/store/single-blog/${blog.id}`}>
+                                                        <img src={BASE_URL + "/storage/media/" + blog.media?.name} className="img-fluid" alt="" />
+                                                    </Link>
+                                                    <Link to={`/store/single-blog/${blog.id}`}>
+                                                        <h2 className="Blog_title">
+                                                            {blog.description.slice(0, 70) + ' ...'}
+                                                        </h2>
+                                                    </Link>
+                                                    <div className="Blog_list">
+                                                        <span className="Blog_author">
+                                                            <i className="fa fa-user"></i>
+                                                            {blog.user.username}
+                                                        </span>
+                                                        <span className="Blog_Date">
+                                                            <i className="fa fa-calendar "></i>
+                                                            {formatDate(blog.created_at)}
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )
+                                    })
+                                }
+
                             </div>
 
                         </div>
 
 
-                    </div>
-                    <div className="col-12 col-lg-3 ">
+                        <BlogSidebar blogs={blogs} />
 
-                        <div className="sidebar_blog">
-                            <div className="widget_blog">
-                                <div className="widget_blog_headbox">
-                                    <h3> دسته بندی ها </h3>
-                                </div>
-                                <div className="blog_list_widget_blog">
-                                    <div className="widget_blog_groups">
-                                        <ul className="widget_blog_posts">
-                                            <li><a className="widget_blog_title_link" href=""> تکنولوژی آینده </a></li>
-                                            <li><a className="widget_blog_title_link" href=""> پرچم دار موبایل </a></li>
-                                            <li><a className="widget_blog_title_link" href=""> ویدئو های کاربردی </a></li>
-                                            <li><a className="widget_blog_title_link" href=""> ترفند های آموزشی </a></li>
-                                            <li><a className="widget_blog_title_link" href=""> دانلود نرم افزار </a></li>
-                                            <li><a className="widget_blog_title_link" href=""> دنیای رمز ارز </a></li>
-                                            <li><a className="widget_blog_title_link" href=""> اخبار روز تکنولوژی </a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="widget_blog marg_top20">
-                                <div className="widget_blog_headbox">
-                                    <h3>آخرین مقالات </h3>
-                                </div>
-                                <div className="blog_list_widget_blog">
-                                    <div className="widget_blog_groups">
-                                        <ul className="widget_blog_posts">
-                                            <li><a className="widget_blog_title_link" href=""> این‌ بار هوش مصنوعی، تصاویری از کارخانه تولید عروس دریایی را افشا می‌کند!</a></li>
-                                            <li><a className="widget_blog_title_link" href="">  خط سفید روی نمایشگر گلکسی A71 سامسونگ؛ ماجرا چیست؟</a></li>
-                                            <li><a className="widget_blog_title_link" href="">بهترین گوشی های مناسب عکاسی در بازار ایران</a></li>
-                                            <li><a className="widget_blog_title_link" href="">هزینه ساخت بازی‌های انحصاری پلی استیشن با فیلم‌های بلاک‌باستر برابری می‌کند</a></li>
-                                            <li><a className="widget_blog_title_link" href=""> یک دهه سکوت شکست؛ مایکروسافت آمار فروش ایکس باکس را فاش کرد!</a></li>
-                                            <li><a className="widget_blog_title_link" href="">  بررسی تلویزیون 4K اسنوا؛ ۵۵ اینچی اقتصادی برای فیلم‌بازها!</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
 
-                        </div>
                     </div>
                 </div>
-            </div>
 
-        </main>
+            </main>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default CategoryBlog
