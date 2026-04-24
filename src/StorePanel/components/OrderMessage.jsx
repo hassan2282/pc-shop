bbbe19b624eb91175c94b433ed1f79736b04f594
hassan2282 <taghavey.hassan@gmail.com>
2026-04-24 16:38:04 +0330

    dynamic order message

--- src/StorePanel/components/OrderMessage.jsx
+++ src/StorePanel/components/OrderMessage.jsx
@@ -1,69 +1,91 @@
-﻿import OrderSideBar from '../structure/OrderSideBar'
+﻿import { useEffect, useState } from 'react'
+import OrderSideBar from '../structure/OrderSideBar'
+import apiClient from '../../apiClient';
+import { toast } from 'react-toastify';
+import { TbLoader } from 'react-icons/tb';
 
 function OrderMessage() {
-  return (
-    <div>
+    const [notifications, setNotifications] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-        <main className="order-delivered  default space-top-30">
-            <div className="container">
-                <div className="row">
-                    <div className="col-xl-9 col-lg-8 col-md-12 order-2">
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <header className="card-header">
-                                    <h3 className="card-title"><span>پیغام‌ها‌</span></h3>
-                                   
-                                </header>
-                                <div className="content-section default">
-                                    <div className="row">
-                                        <div className="col-md-6 col-sm-6 order_delivered_sec">
-                                            <div className="row">
-                                                <div className="col-12 col-lg-12 col-md-12">
+    useEffect(() => {
+        setIsLoading(true)
+        const fetchUserNotify = async () => {
+            try {
+                const res = await apiClient.get('/user-notify');
+                const { data, status } = res;
+                if (status >= 200 && status < 300) {
+                    setNotifications(data);
+                }
+            } catch (err) {
+                toast.error('خطا در فرآیند واکشی نوتیفیکیشن ها');
+            } finally {
+                setIsLoading(false);
+            }
+        }
 
-                                                    <h4 className="profile-recent-fav-name">
-                                                        <i className="fa fa-bell " aria-hidden="true"></i> سفارش شما با کد 45879625 ثبت شده و در حال پردازش است
-                                                    </h4>
-                                                    <span className="span-sub-bel">۲ اردیبهشت ۱۴۰۲</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6 col-sm-6 order_delivered_sec">
-                                            <div className="row">
-                                                <div className="col-12 col-lg-12 col-md-12">
+        fetchUserNotify();
+    }, []);
 
-                                                    <h4 className="profile-recent-fav-name">
-                                                        <i className="fa fa-bell " aria-hidden="true"></i>      تجربه خریدت برامون مهمه
-                                                    </h4>
-                                                    <span className="span-sub-bel">25 اردیبهشت ۱۴۰۲</span>
+    const formatDate = (date) => {
+        return new Intl.DateTimeFormat("fa-IR-u-ca-persian", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+        }).format(new Date(date));
+    };
 
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6 col-sm-6 order_delivered_sec">
-                                            <div className="row">
-                                                <div className="col-12 col-lg-12 col-md-12">
+    return (
+        <div>
 
-                                                    <h4 className="profile-recent-fav-name">
-                                                        <i className="fa fa-bell " aria-hidden="true"></i>مبلغ سفارش لغو شده به حساب شما واریز شد
-                                                    </h4>
-                                                    <span className="span-sub-bel">۲ اسفند 1401</span>
+            <main className="order-delivered  default space-top-30">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xl-9 col-lg-8 col-md-12 order-2">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <header className="card-header">
+                                        <h3 className="card-title"><span>پیغام‌ها‌</span></h3>
 
+                                    </header>
+                                    <div className="content-section default">
+                                        <div className="row">
+                                            {
+                                                isLoading && (
+                                                    <TbLoader size={25} className='text-[#5CBABF] animate-spin' />
+                                                )
+                                            }
+                                            {
+                                                notifications &&
+                                                notifications.map((notif, index) => {
+                                                    return (
+                                                        <div key={index} className="col-md-12 col-sm-12 order_delivered_sec">
+                                                            <div className="row">
+                                                                <div className="col-12 col-lg-12 col-md-12">
 
-                                                </div>
-                                            </div>
+                                                                    <h4 className="profile-recent-fav-name">
+                                                                        <i className="fa fa-bell " aria-hidden="true"></i>
+                                                                        {notif.data.body}
+                                                                    </h4>
+                                                                    <span className="span-sub-bel">{formatDate(notif.created_at)}</span>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                })
+                                            }
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        <OrderSideBar />
                     </div>
-                    <OrderSideBar />
                 </div>
-            </div>
-        </main>
+            </main>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default OrderMessage
