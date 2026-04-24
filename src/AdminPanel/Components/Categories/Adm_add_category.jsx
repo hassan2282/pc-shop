8d3de8ed8d48272c117bbfb32fe272354b010468
hassan2282 <taghavey.hassan@gmail.com>
2026-04-24 16:33:19 +0330

    debug route in adm add category

--- src/AdminPanel/Components/Categories/Adm_add_category.jsx
+++ src/AdminPanel/Components/Categories/Adm_add_category.jsx
@@ -21,7 +21,7 @@ function Adm_add_category() {
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const fetchResponse = await apiClient.get('/admin/category/all');
+        const fetchResponse = await apiClient.get('/category/all');
         if (fetchResponse.status >= 200 && fetchResponse.status < 300) {
           setCategories(fetchResponse.data);
         }
