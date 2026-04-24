0d3b2423a49b8c1224744aaf3ce7cd7f9e67d008
hassan2282 <taghavey.hassan@gmail.com>
2026-04-24 16:32:43 +0330

    improve status filter component ui

--- src/AdminPanel/Components/StatusFilter.jsx
+++ src/AdminPanel/Components/StatusFilter.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function StatusFilter({ statusChangeFilter }) {
   return (
-    <select onChange={(e) => statusChangeFilter(e)} className='border-white/90 border-1 bg-white/80 cursor-pointer rounded-xl p-2'>
+    <select onChange={(e) => statusChangeFilter(e)} 
+    className='border-white/90 border-1 bg-white/80
+     cursor-pointer rounded-xl p-2 focus:outline-0 focus:ring-2 focus:ring-blue-600'>
       <option value={''}>وضعیت</option>
       <option value={1}>فعال</option>
       <option value={0}>غیر فعال</option>
