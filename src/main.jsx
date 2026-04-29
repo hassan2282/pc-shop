016422503364493d6489f4c80d150efeb400e962
hassan2282 <taghavey.hassan@gmail.com>
2026-04-29 19:38:03 +0330

    add react-query over all app

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import './index.css'
 import App from './App.jsx'
 import ReactDOM from "react-dom/client";
 import { Provider } from 'react-redux'
-import React from 'react';
 import store from './store.js';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </QueryClientProvider>
 );
